09374e883f267e460dfa4bab8ba0a9ac9e8a0c73
rexx0n <alexey.mironenko46@gmail.com>
2023-05-23 19:14:19 +0300

    made function generetePin and made CheckPin

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 
-const supabase = createClient('https://hbfmnhsfdoztjbpwiazd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiZm1uaHNmZG96dGpicHdpYXpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjI1Njc1MDUsImV4cCI6MTk3ODE0MzUwNX0.q55-HVNv0f8V5y8vVhZ4XwI5o8FvAy0QW7PtB10v4Ws')
+const supabase = createClient('https://huqamqzpidbjfvpoqdti.supabase.co/', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1cWFtcXpwaWRiamZ2cG9xZHRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4NDA0NzUsImV4cCI6MjAwMDQxNjQ3NX0.X0jVOJpS1fI3bnjkcWHWQhMloHgXxGXWrFsI-SHJObs')
 export default supabase
--- src/pages/CreateOrJoinPage.vue
+++ src/pages/CreateOrJoinPage.vue
@@ -5,17 +5,12 @@
             <ul>
                 <li v-for="quizz in quizzes" :key="quizz.id">
 <!--                    <a href=""></a>-->
-                    <router-link to="/username" @click="onClick(quizz.id)">
+                    <router-link to="/gameHost" @click="onClick(quizz.id)">
                         {{quizz.title}}
                     </router-link>
                 </li>
             </ul>
             <div class="buttons">
-                    <MyButton>
-                        <router-link to="/roomHost" @click="onClickBtn('host')">
-                            Создать
-                        </router-link>
-                    </MyButton>
                     <MyButton>
                         <router-link to="/username" @click="onClickBtn('client')">
                         Присоединиться
@@ -32,10 +27,42 @@ import {onMounted, ref} from "vue";
 import {useQuizStore} from "@/store/useQuizStore";
 const quizzes = ref()
 const {store} = useQuizStore()
+const pin = ref()
+const roomPin = ref()
+//todo
+//сделать генерацию 8-значного пина уникального
+//
 
-function onClick(id) {
-    store.quizId = id
+function generatePin () {
+    const generatedPin = Math.floor(10000000 + Math.random() * 90000000)
+    pin.value = generatedPin;
 }
+async function onClick(id) {
+    store.quizId = id
+    generatePin()
+    await checkPin()
+    const {data, error} = await supabase
+        .from('room')
+        .insert([
+            {pin: pin.value, quiz_id: store.quizId},
+        ])
+}
+async function checkPin() {
+    let {data: room, error} = await supabase
+        .from('room')
+        .select('pin')
+        roomPin.value = room
+    for (let i = 0; i < roomPin.value.length; i++) {
+        if (pin.value === roomPin.value[i].pin) {
+            pin.value = generatePin()
+            console.log(false)
+        }
+        else {
+            console.log(true)
+        }
+    }
+}
+
 function onClickBtn (hostOrClient) {
     store.user = hostOrClient
 }
